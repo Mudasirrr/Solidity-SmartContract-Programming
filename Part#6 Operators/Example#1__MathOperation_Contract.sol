d62017ea572b22e539a0771b38151287e4846c92
Mudasir Hussain <40722738+Mudasirrr@users.noreply.github.com>
2020-06-28 11:13:24 +0500

    Add files via upload

---
+++ Part#6 Operators/Example#1__MathOperation_Contract.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.25;
+/*
+Math Operations
+Math in Solidity is pretty straightforward. The following operations are the same as in most programming languages:
+
+Addition: x + y
+Subtraction: x - y,
+Multiplication: x * y
+Division: x / y
+Modulus / remainder: x % y (for example, 13 % 5 is 3, because if you divide 5 into 13, 3 is the remainder)
+Solidity also supports an exponential operator (i.e. "x to the power of y", x^y):
+
+uint x = 5 ** 2; // equal to 5^2 = 25
+*/
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    //start here
+uint dnaModulus=10**dnaDigits;
+}
